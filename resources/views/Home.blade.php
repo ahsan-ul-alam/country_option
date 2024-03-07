37c8237172c6862b1cea49ba9409f5bd887901e9
Ahsan-ul-alam <ahsanulalam.500@gmail.com>
2024-03-08 00:36:06 +0600

    js involved

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,5 +1,18 @@
-<select>
+<select id="country">
     @foreach ($list as $item)
         <option value="{{ $item }}">{{ $item }}</option>
     @endforeach
 </select>
+<button id="showbtn">
+    show
+</button>
+<h2 id="display"></h2>
+
+
+<script>
+    document.getElementById("showbtn").addEventListener("click",(event)=>{
+        event.preventDefault();
+        let country = document.getElementById('country').value;
+        document.getElementById("display").innerText = country;
+    })
+</script>
